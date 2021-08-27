d147d1d8c69466b29d6d3c1da5a6940b25960659
menegais <menegais1@gmail.com>
2021-08-27 19:42:13 -0300

    Add base A* algorithm for every agent and correct renderization based on steps

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 #include "FileManagers/ModelLoader.h"
 #include <imgui.h>
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/norm.hpp>
 #include <iostream>
 #include <random>
 
@@ -131,6 +132,19 @@ struct Transform {
     glm::vec3 scale;
 
     Transform(const glm::vec3 &position, const glm::vec3 &rotation, const glm::vec3 &scale) : position(position), rotation(rotation), scale(scale) {}
+
+    glm::mat4x4 getModelMatrix() {
+        if (std::abs(rotation.x) >= 360) rotation.x = (int) rotation.x % 360;
+        if (std::abs(rotation.y) >= 360) rotation.y = (int) rotation.y % 360;
+        if (std::abs(rotation.z) >= 360) rotation.z = (int) rotation.z % 360;
+        auto S = glm::scale(glm::identity<glm::mat4x4>(), scale);
+        auto T = glm::translate(glm::identity<glm::mat4x4>(), position);
+        auto Rx = glm::rotate(glm::identity<glm::mat4x4>(), (float) (rotation.x * PI / 180.0f), glm::vec3(1, 0, 0));
+        auto Ry = glm::rotate(glm::identity<glm::mat4x4>(), (float) (rotation.y * PI / 180.0f), glm::vec3(0, 1, 0));
+        auto Rz = glm::rotate(glm::identity<glm::mat4x4>(), (float) (rotation.z * PI / 180.0f), glm::vec3(0, 0, 1));
+        glm::mat4x4 M = T * Rz * Ry * Rx * S;
+        return M;
+    }
 };
 
 class Mesh {
@@ -141,9 +155,16 @@ public:
     glm::vec3 maxBound;
     glm::vec3 size;
 
-    Mesh(std::string fileName) {
+    Mesh(std::string fileName, Transform transform) {
         ModelLoader::loadPnuModel(FileLoader::getPath(fileName), vertices, indices);
+        auto M = transform.getModelMatrix();
+
+        for (auto &vertex: vertices) {
+            vertex.position = M * glm::vec4(vertex.position, 1);
+        }
+
         recalculateBounds();
+
     }
 
     void recalculateBounds() {
@@ -195,20 +216,9 @@ public:
         glBindVertexArray(0);
     }
 
-    void update() {
-
-    }
 
     void render() {
-        if (std::abs(transform.rotation.x) >= 360) transform.rotation.x = (int) transform.rotation.x % 360;
-        if (std::abs(transform.rotation.y) >= 360) transform.rotation.y = (int) transform.rotation.y % 360;
-        if (std::abs(transform.rotation.z) >= 360) transform.rotation.z = (int) transform.rotation.z % 360;
-        auto S = glm::scale(glm::identity<glm::mat4x4>(), transform.scale);
-        auto T = glm::translate(glm::identity<glm::mat4x4>(), transform.position);
-        auto Rx = glm::rotate(glm::identity<glm::mat4x4>(), (float) (transform.rotation.x * PI / 180.0f), glm::vec3(1, 0, 0));
-        auto Ry = glm::rotate(glm::identity<glm::mat4x4>(), (float) (transform.rotation.y * PI / 180.0f), glm::vec3(0, 1, 0));
-        auto Rz = glm::rotate(glm::identity<glm::mat4x4>(), (float) (transform.rotation.z * PI / 180.0f), glm::vec3(0, 0, 1));
-        M = T * Rz * Ry * Rx * S;
+        M = transform.getModelMatrix();
 
         glBindVertexArray(VAO);
         defaultShader.activateShader();
@@ -222,13 +232,32 @@ private:
 
 };
 
+glm::ivec3 getGridIndex(int index, glm::vec3 gridSize) {
+    int indexZ = index / (int) (gridSize.x * gridSize.y);
+    int indexY = index % (int) (gridSize.x * gridSize.y) / (int) gridSize.x;
+    int indexX = index % (int) (gridSize.x * gridSize.y) % (int) gridSize.x;
+    return glm::ivec3(indexX, indexY, indexZ);
+}
+
+int getLinearIndex(glm::ivec3 index, glm::vec3 gridSize) {
+    int linearIndex = index.z * (gridSize.x * gridSize.y) + index.y * gridSize.y + index.x;
+    if (linearIndex > gridSize.x * gridSize.y * gridSize.z) {
+        std::cerr << "AN ERROR HAS OCCURED" << std::endl;
+        exit(-1);
+    }
+    return linearIndex;
+}
+
 std::vector<int> discretize(Mesh *mesh, glm::vec3 gridSize) {
     std::vector<int> validIndices;
     auto cellSize = mesh->size / gridSize;
     for (PnuVertexInput vertex: mesh->vertices) {
         auto position = vertex.position - mesh->minBound;
         glm::ivec3 index = position / cellSize;
-        int linearIndex = index.z * (gridSize.x * gridSize.y) + index.y * gridSize.y + index.x;
+        if (index.x >= gridSize.x)index.x = gridSize.x - 1;
+        if (index.y >= gridSize.y)index.y = gridSize.y - 1;
+        if (index.z >= gridSize.z)index.z = gridSize.z - 1;
+        int linearIndex = getLinearIndex(index, gridSize);
         validIndices.push_back(linearIndex);
     }
     std::vector<int>::iterator it;
@@ -237,7 +266,7 @@ std::vector<int> discretize(Mesh *mesh, glm::vec3 gridSize) {
     return validIndices;
 }
 
-std::vector<int> assignGoals(int agentsNumber, std::vector<int> validGoals) {
+std::vector<int> assignRandomGoals(int agentsNumber, std::vector<int> validGoals) {
     std::random_device rd;
     std::mt19937 g(rd());
     std::shuffle(validGoals.begin(), validGoals.end(), g);
@@ -247,6 +276,176 @@ std::vector<int> assignGoals(int agentsNumber, std::vector<int> validGoals) {
     }
     return goals;
 }
+
+std::vector<int> assignClosestGoals(std::vector<int> startPositions, std::vector<int> validGoals, glm::vec3 gridSize) {
+    std::vector<int> goals;
+
+    for (int i = 0; i < startPositions.size(); i++) {
+        glm::vec3 position = getGridIndex(startPositions[i], gridSize);
+        float closestDist = std::numeric_limits<float>::max();
+        int closestGoal = -1;
+        for (int j = 0; j < validGoals.size(); ++j) {
+            glm::vec3 goalPosition = getGridIndex(validGoals[j], gridSize);
+            auto distance = glm::distance(position, goalPosition);
+            if (distance < closestDist) {
+                closestDist = distance;
+                closestGoal = j;
+            }
+        }
+        goals.push_back(validGoals[closestGoal]);
+        validGoals.erase(validGoals.begin() + closestGoal);
+    }
+    return goals;
+}
+
+struct Node {
+public:
+    Node *parent;
+    float g_cost;
+    float h_cost;
+    glm::ivec3 index;
+    std::vector<Node *> neighbours;
+    glm::vec3 position;
+
+    Node(Node *parent, glm::ivec3 index, glm::vec3 position) : parent(parent), index(index), position(position) {
+        reset();
+    }
+
+    float getFCost() {
+        return g_cost + h_cost;
+    }
+
+    void reset() {
+        parent = nullptr;
+        g_cost = std::numeric_limits<float>::max();
+        h_cost = std::numeric_limits<float>::max();
+    }
+};
+
+std::vector<Node *> getNeighbours(std::unordered_map<glm::ivec3, Node *> &nodes, Node *node, glm::vec3 gridSize) {
+    std::vector<Node *> neighbours;
+    auto nodeIndex = node->index;
+    std::vector<glm::ivec3> indices;
+    indices.push_back(nodeIndex + glm::ivec3(-1, 0, 0));
+    indices.push_back(nodeIndex + glm::ivec3(1, 0, 0));
+    indices.push_back(nodeIndex + glm::ivec3(0, 1, 0));
+    indices.push_back(nodeIndex + glm::ivec3(0, -1, 0));
+    indices.push_back(nodeIndex + glm::ivec3(0, 0, 1));
+    indices.push_back(nodeIndex + glm::ivec3(0, 0, -1));
+
+    for (auto index: indices) {
+        if (nodes.contains(index)) {
+            neighbours.push_back(nodes[index]);
+        }
+    }
+
+    return neighbours;
+}
+
+Node *getSmallestCostNode(std::vector<Node *> &nodes) {
+    float leastCost = std::numeric_limits<float>::max();
+    Node *leastCostNode;
+    for (auto node: nodes) {
+        if (node->getFCost() < leastCost) {
+            leastCost = node->getFCost();
+            leastCostNode = node;
+        }
+    }
+
+    return leastCostNode;
+}
+
+std::vector<glm::ivec3> getAStarPath(Node *endNode) {
+    std::vector<glm::ivec3> indices;
+    indices.push_back(endNode->index);
+    Node *curNode = endNode->parent;
+    while (curNode != nullptr) {
+        indices.push_back(curNode->index);
+        curNode = curNode->parent;
+    }
+    std::reverse(indices.begin(), indices.end());
+    return indices;
+}
+
+float l1_norm(Node *current, Node *last) {
+    return glm::l1Norm(current->position, last->position);
+}
+
+void resetNodes(std::unordered_map<glm::ivec3, Node *> &nodes) {
+    for (auto item: nodes) {
+        item.second->reset();
+    }
+}
+
+std::vector<glm::ivec3> pathFindingAStar(Node *startNode, Node *endNode) {
+    std::vector<Node *> open;
+    std::vector<Node *> closed;
+
+    startNode->h_cost = l1_norm(startNode, endNode);
+    startNode->g_cost = 0;
+    open.push_back(startNode);
+    Node *current;
+    while (open.size() > 0) {
+        current = getSmallestCostNode(open);
+
+        if (current == endNode) return getAStarPath(current);
+        auto it = std::remove(open.begin(), open.end(), current);
+        open.resize(std::distance(open.begin(), it));
+        closed.push_back(current);
+        for (auto neighbour: current->neighbours) {
+            if (std::find(closed.begin(), closed.end(), neighbour) != closed.end()) continue;
+            float cost = glm::l1Norm(neighbour->position, current->position) + current->g_cost;
+            bool notInOpen = std::find(open.begin(), open.end(), neighbour) == open.end();
+            if (cost < neighbour->g_cost || notInOpen) {
+                neighbour->g_cost = cost;
+                neighbour->h_cost = l1_norm(neighbour, endNode);
+                neighbour->parent = current;
+
+                if (notInOpen) {
+                    open.push_back(neighbour);
+                }
+            }
+        }
+    }
+
+    return getAStarPath(current);
+}
+
+class MAPF : public GameObject {
+public:
+    std::vector<MeshRenderer *> agents;
+    std::vector<std::vector<glm::ivec3>> paths;
+    int step = 0;
+    Mesh *mesh;
+    glm::vec3 cellSize;
+    int finalStep = -1;
+
+    MAPF(const std::vector<MeshRenderer *> &agents, const std::vector<std::vector<glm::ivec3>> &paths, Mesh *mesh, glm::vec3 cellSize) : agents(agents), paths(paths),
+                                                                                                                                         mesh(mesh), cellSize(cellSize) {
+        for (const auto &path: paths) {
+            if (int(path.size() - 1) > finalStep) {
+                finalStep = int(path.size()) - 1;
+            }
+        }
+
+    }
+
+    void keyboard(int key, int scancode, int action, int mods) override {
+        if (key == GLFW_KEY_RIGHT && action == GLFW_RELEASE)step++;
+        if (key == GLFW_KEY_LEFT && action == GLFW_RELEASE) step--;
+
+        if (step < 0) step = 0;
+        if (step > finalStep) step = finalStep;
+        for (int i = 0; i < agents.size(); ++i) {
+            auto path = paths[i];
+            if (step >= path.size()) continue;
+            auto gridIndex = path[step];
+            agents[i]->transform.position = glm::vec3(mesh->minBound.x + (gridIndex.x * cellSize.x) + (cellSize.x / 2),
+                                                      mesh->minBound.y + (gridIndex.y * cellSize.y) + (cellSize.y / 2),
+                                                      mesh->minBound.z + (gridIndex.z * cellSize.z) + (cellSize.z / 2));
+        }
+    }
+};
 
 void init() {
 
@@ -258,25 +457,65 @@ void init() {
     defaultShader = Shader(vertex, fragment);
 
     camera = new Camera(glm::vec3(0, 0, 1), glm::vec3(0, 0, -1), glm::vec3(0, 1, 0));
-    Mesh *mesh = new Mesh("Resources/Meshes/ufsm.obj");
-    Mesh *cube = new Mesh("Resources/Meshes/Cube.obj");
+    Mesh *mesh = new Mesh("Resources/Meshes/IA.obj", Transform({0, 0, 0}, {180, 0, 0}, {1, 1, 1}));
+    Mesh *cube = new Mesh("Resources/Meshes/Cube.obj", Transform({0, 0, 0}, {0, 0, 0}, {1, 1, 1}));
 //    MeshRenderer *meshRenderer = new MeshRenderer(Transform({0, 0, 0}, {0, 0, 0}, {1, 1, 1}), mesh);
+    glm::vec3 cellSize = mesh->size / gridSize;
+
+
+    //Create graph structure and add neighbours
+    std::cout << "Generating Nodes in Graph" << std::endl;
+    std::unordered_map<glm::ivec3, Node *> nodes;
+    for (int x = 0; x < gridSize.x; ++x) {
+        for (int y = 0; y < gridSize.y; ++y) {
+            for (int z = 0; z < gridSize.z; ++z) {
+                glm::ivec3 index = glm::ivec3(x, y, z);
+                glm::vec3 position = glm::vec3(mesh->minBound.x + (index.x * cellSize.x) + (cellSize.x / 2),
+                                               mesh->minBound.y + (index.y * cellSize.y) + (cellSize.y / 2),
+                                               mesh->minBound.z + (index.z * cellSize.z) + (cellSize.z / 2));
+                nodes[index] = new Node(nullptr, index, position);
+            }
+        }
+    }
+    std::cout << "Setting neighbours in Graph" << std::endl;
+    for (auto item: nodes) {
+        Node *node = item.second;
+        node->neighbours = getNeighbours(nodes, node, gridSize);
+    }
+    //End creation
 
 
     auto validIndices = discretize(mesh, gridSize);
-    glm::vec3 cellSize = mesh->size / gridSize;
 
-    auto goals = assignGoals(agentsNumber, validIndices);
 
-    for (auto index: goals) {
-        int indexZ = index / (int) (gridSize.x * gridSize.y);
-        int indexY = index % (int) (gridSize.x * gridSize.y) / (int) gridSize.x;
-        int indexX = index % (int) (gridSize.x * gridSize.y) % (int) gridSize.x;
-        MeshRenderer *meshRenderer = new MeshRenderer(Transform({mesh->minBound.x + (indexX * cellSize.x) + (cellSize.x / 2),
-                                                                 mesh->minBound.y + (indexY * cellSize.y) + (cellSize.y / 2),
-                                                                 mesh->minBound.z + (indexZ * cellSize.z) + (cellSize.z / 2)},
-                                                                {0, 0, 0}, {0.01, 0.01, 0.01}), cube);
+    std::vector<int> start;
+    for (int i = 0; i < agentsNumber; ++i) {
+        start.push_back(rand() % (int) (gridSize.x * gridSize.y * gridSize.z));
     }
+
+//    auto goals = assignRandomGoals(agentsNumber, validIndices);
+    auto goals = assignClosestGoals(start, validIndices, gridSize);
+
+    std::vector<MeshRenderer *> agents;
+    for (auto index: start) {
+        glm::ivec3 gridIndex = getGridIndex(index, gridSize);
+        agents.push_back(new MeshRenderer(Transform({mesh->minBound.x + (gridIndex.x * cellSize.x) + (cellSize.x / 2),
+                                                     mesh->minBound.y + (gridIndex.y * cellSize.y) + (cellSize.y / 2),
+                                                     mesh->minBound.z + (gridIndex.z * cellSize.z) + (cellSize.z / 2)},
+                                                    {0, 0, 0}, {0.01, 0.01, 0.01}), cube));
+    }
+
+    std::vector<std::vector<glm::ivec3>> paths;
+    for (int i = 0; i < agentsNumber; ++i) {
+        auto startIndex = getGridIndex(start[i], gridSize);
+        auto endIndex = getGridIndex(goals[i], gridSize);
+        auto path = pathFindingAStar(nodes[startIndex], nodes[endIndex]);
+        paths.push_back(path);
+        std::cout << "Agent: " << i << " has " << path.size() << std::endl;
+        resetNodes(nodes);
+    }
+
+    MAPF *mapf = new MAPF(agents, paths, mesh, cellSize);
 }
 
 void Callbacks::mouseMovement(double x, double y) {
